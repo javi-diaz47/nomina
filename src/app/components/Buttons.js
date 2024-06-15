2e094cb3b2a5261e9dc464624e7e4b2c22ab000b
javi-diaz47 <javiereduardo300@gmail.com>
2024-06-15 17:00:36 -0500

    feat: add navbar in home

--- src/app/components/Buttons.js
+++ src/app/components/Buttons.js
@@ -1,3 +1,14 @@
-export const Button = ({ title }) => {
-  return <button>{title}</button>
+import { twMerge } from 'tailwind-merge'
+
+export const Button = ({ className, children }) => {
+  return (
+    <div
+      className={twMerge(
+        'w-48 px-6 py-2 font-bold text-black bg-white capitalize transition-transform ease-in',
+        'hover:scale-105',
+        className
+      )}>
+      {children}
+    </div>
+  )
 }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,7 +5,6 @@ export default function Login() {
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         login
-        <Button title={'Click me'} />
       </div>
     </main>
   )
